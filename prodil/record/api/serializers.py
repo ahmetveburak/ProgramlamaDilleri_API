11b187c60e821814bc76beab9ac680f38574a224
ahmetveburak <ahmetbozyurtt@gmail.com>
2021-10-30 23:31:31 +0300

    add authorserializer

--- prodil/record/api/serializers.py
+++ prodil/record/api/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
-from prodil.record.models import Category, Resource
+from prodil.record.models import Author, Category, Resource
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = (
+            "full_name",
+            "site",
+        )
 
 
 class ResourceSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(many=True)
+
     class Meta:
         model = Resource
         fields = (
