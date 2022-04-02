902b61f88b2c52adeb10d38414959d5f1767f0b1
ahmetveburak <ahmetbozyurtt@gmail.com>
2022-04-02 13:22:08 +0300

    add django-environ

--- prodil/settings.py
+++ prodil/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+env.read_env(".env")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$8t!v$!1lra)duz@35dojvzj@f4_1zw=sj&0e*t%*dn_d+c$qf"
+SECRET_KEY = env.str("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "api"]
 
 
 # Application definition
@@ -87,8 +92,12 @@ WSGI_APPLICATION = "prodil.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DB_NAME"),
+        "HOST": env.str("DB_HOST"),
+        "PASSWORD": env.str("DB_PASSWORD"),
+        "PORT": env.str("DB_PORT"),
+        "USER": env.str("DB_USER"),
     }
 }
 
